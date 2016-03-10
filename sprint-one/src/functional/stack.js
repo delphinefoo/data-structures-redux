d3bed1dc86c5194ef931ebcaf70179775895af14
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-10 09:29:56 -0800

    Completed functional instantiation of stack and queue.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function(){
+    var removed = storage[counter-1];
+    delete storage[counter-1];
+    if (counter > 0) counter--;
+    return removed;
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
