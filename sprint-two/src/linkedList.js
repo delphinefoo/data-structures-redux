594042abc1e413c515c16f82c3074a7be8c04756
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-23 13:58:53 -0700

    Completed linked list implementation.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = new Node(value);
+    if(!list.head) {
+      list.head = newNode;
+    }
+    if(list.tail) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var removed = list.head;
+    if(list.head) {
+      list.head = list.head.next;
+    }
+    if (list.head === list.tail) {
+      list.tail = null;
+    }
+    return removed.value;
   };
 
   list.contains = function(target){
+    var subroutine = function(node) {
+      if(node.value === target) return true;
+      if(node.next === null) return false;
+      return subroutine(node.next);
+    }
+    return subroutine(list.head);
   };
 
   return list;
