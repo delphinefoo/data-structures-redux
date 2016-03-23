bcc6e10c42c4db28961628937d36e524bcc27dad
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-23 13:13:32 -0700

    Completed prototypal instantiation pattern for stacks.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance._storage = {};
+  someInstance._size = 0;
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
+  this._storage[this._size] = val;
+  this._size++;
+};
+
+stackMethods.pop = function() {
+  var removed = this._storage[this._size - 1];
+  delete this._storage[this._size - 1];
+  if (this._size > 0) this._size--;
+  return removed;
+};
+
+stackMethods.size = function() {
+  return this._size;
+};
 
