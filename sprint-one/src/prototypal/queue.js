9fdf18af69e9e53442714f79a1c16a197d646652
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-23 13:17:30 -0700

    Completed prototypal inheritance pattern for queues.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance._storage = {};
+  someInstance._size = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val) {
+  this._storage[this._size] = val;
+  this._size++;
+};
+
+queueMethods.dequeue = function() {
+  var removed = this._storage[0];
+  delete this._storage[0];
+  for (var key in this._storage) {
+    this._storage[key - 1] = this._storage[key];
+  }
+  if (this._size > 0) this._size--;
+  return removed;
+};
+
+queueMethods.size = function() {
+  return this._size;
+};
 
