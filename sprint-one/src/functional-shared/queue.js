38a5272a50fda93c85e7ebed3ecf2c2af2cba59f
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-23 13:06:20 -0700

    Completed functional shared instantiation of stacks and queues.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue._storage = {};
+  newQueue._size = 0;
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+queueMethods.dequeue = function() {
+  var removed = this._storage[0];
+  delete this._storage[0];
+  for (var key in this._storage) {
+    this._storage[key - 1] = this._storage[key];
+  }
+  if (this._size > 0) this._size--;
+  return removed;
+};
+
+queueMethods.size = function() {
+  return this._size;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack._storage = {};
+  newStack._size = 0;
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+stackMethods.pop = function() {
+  var removed = this._storage[this._size - 1];
+  delete this._storage[this._size - 1];
+  if (this._size > 0) this._size--;
+  return removed;
+};
+
+stackMethods.size = function() {
+  return this._size;
+};
+
+
 
 
