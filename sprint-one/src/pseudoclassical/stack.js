fbe7d273c154bae47dc8f61c6228ad59d019464c
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-23 13:21:19 -0700

    Completed pseudoclassical instantiation pattern for stacks.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._size = 0;
 };
 
+Stack.prototype.push = function(val) {
+  this._storage[this._size] = val;
+  this._size++;
+};
 
+Stack.prototype.pop = function() {
+  var removed = this._storage[this._size - 1];
+  delete this._storage[this._size - 1];
+  if(this._size > 0) this._size--;
+  return removed;
+};
+
+Stack.prototype.size = function() {
+  return this._size;
+};
