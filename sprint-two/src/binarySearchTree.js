0b43c5e04aeb27591de19801c28960d7a4e3af60
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-27 11:28:07 -0700

    Implemented  insert function for binary search tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,35 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (value < this.value) {
+    //insert on left
+    if (!this.left) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (!this.right) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
+
+
 
 
 /*
